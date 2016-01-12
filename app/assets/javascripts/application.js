05b9a3dcd93fb257392a805e002617f67a07f77b
MiaKoda Grodsky <mcgrodsky@gmail.com>
2016-01-12 09:10:04 -0500

    chords are generated through button click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,40 +17,176 @@
 
 console.log("haaaay")
 $( document ).ready(function() {
-  var strings = ["E", "A", "D", "G", "B", "e"];
-  var frets = 14;
-
+  var strings = ["e", "b", "G", "D", "A", "E"];
+  var dots = [3,5,7,9,15,17];
+  var doubleDots = 12;
+  var nextDot = dots.shift();
+  var frets = 15;
 
   for(var f = 0; f < frets; f++) {
     // create a new row that will represent this fret level
     var $fret = $("<div class='fret' data-fret='"+f+"'/>");
 
     for (i = 0; i < strings.length; i++) {
-      var $div = $("<div/>");
-      var $input = $("<input type='checkbox'>");
+      // set id to strings[i]+fret num
+      var id = strings[i] + f;
+      var $div = $("<div class='note'/>");
+      $div.attr('data-string', strings[i]);
+      var $input = $("<input type='checkbox' title='"+id+"'>");
       $input.attr("data-string", strings[i]);
       // use f as the fret number
       $input.attr("data-fret", f);
-      // set id to strings[i]+fret num
-      var id = strings[i] + f;
       $input.attr("id", id);
       $div.append($input);
-      // use id again to set 'for' equal to id
+
+      // use id again
       var $label = $("<label for='"+id+"'></label>");
+
       $div.append($input);
       $div.append($label);
       $fret.append($div); // add to the fret, not the fretboard
 
     }
-  console.log("here");
+
+    if(f === doubleDots) {
+      $fret.addClass('two-dots');
+    }
+    else if(f === nextDot) {
+      nextDot = dots.shift();
+      $fret.addClass('dot');
+    }
+
     // done creating the row, now append that to the fretboard
     $('.fretboard').append($fret);
   }
 
-  //function for barre chords
-  //for every input-type = data-string E, change input to X
-  $('.Estring').on("click", function(){
-    console.log ($("[data-string='E'] [label]"));
+  // list the notes in a chord represented by a button element
+  var chords = {
+    E: ['E0','A2','D2','G1','b0','e0'],
+    Em: ['E0','A2','D2','G0','b0','e0'],
+    D: ['D0','G2','b3','e2'],
+    D_bar: ['A5','D7','G7','b7','e5'],
+    A: ['e0','A0','b2','G2','D2']
+  }
 
-  })
+  $("button").on('click', function (e) {
+    // when a button is clicked, show which notes are pressed for that chord
+    var chord = chords[$(this).attr('data-chord')];
+    if(chord) {
+      $(".fretboard input").prop('checked', false);
+      for(var i=0; i < chord.length; i++) {
+        $(".fretboard input#"+chord[i]).prop('checked', true);
+      }
+    }
+    showSelected();
+  });
+  $(".fretboard").on('change', 'input', function (e) {
+    // when an input is changed, update the list of pressed notes and show the updated list
+    clearOthersInRow(this);
+    showSelected();
+  });
+  function showSelected() {
+    // output a list of strings being pressed (for convenience in appending to chords array)
+    var sel = getSelected();
+    $("#selected").val(sel.length?"'"+sel.join("','")+"'":'');
+  }
+  function getSelected() {
+    // get the array of notes being pressed
+    var sel = [];
+    $('.fretboard input:checked').each(function () {
+      sel.push($(this).attr('id'));
+    });
+    return sel;
+  }
+  function clearOthersInRow(selected) {
+    // ensure `selected` is the only note being pressed on this string
+    var s = $(selected).attr('data-string');
+    $('.fretboard div[data-string="'+s+'"] input')
+        .not(selected).prop('checked', false);
+  }
+
+  // initialize
+  showSelected();
+
 });
+
+
+
+// var strings = ["e", "b", "G", "D", "A", "E"];
+// var frets = 15;
+//
+// for(var f = 0; f < frets; f++) {
+//   // create a new row that will represent this fret level
+//   var $fret = $("<div class='fret' data-fret='"+f+"'/>");
+//
+//   for (i = 0; i < strings.length; i++) {
+//     // set id to strings[i]+fret num
+//     var id = strings[i] + f;
+//     var $div = $("<div class='note'/>");
+//     $div.attr('data-string', strings[i]);
+//     var $input = $("<input type='checkbox' title='"+id+"'>");
+//     $input.attr("data-string", strings[i]);
+//     // use f as the fret number
+//     $input.attr("data-fret", f);
+//     $input.attr("id", id);
+//     $div.append($input);
+//
+//     // use id again to connect for and id
+//     var $label = $("<label for='"+id+"'></label>");
+//
+//     $div.append($input);
+//     $div.append($label);
+//     $fret.append($div); // add to the fret, not the fretboard
+//   }
+//   // done creating the row, now append that to the fretboard
+//   $('.fretboard').append($fret);
+// }
+//
+// // list the notes in a chord represented by a button element
+// var chords = {
+//   E: ['E0','A2','D2','G1','b0','e0'],
+//   Em: ['E0','A2','D2','G0','b0','e0'],
+//   D: ['D0','G2','b3','e2'],
+//   D_bar: ['A5','D7','G7','b7','e5'],
+//   A: ['e0','A0','b2','G2','D2']
+// }
+//
+// $("button").on('click', function (e) {
+//   // when a button is clicked, show which notes are pressed for that chord
+//   var chord = chords[$(this).attr('data-chord')];
+//   if(chord) {
+//     $(".fretboard input").prop('checked', false);
+//     for(var i=0; i < chord.length; i++) {
+//       $(".fretboard input#"+chord[i]).prop('checked', true);
+//     }
+//   }
+//   showSelected();
+// });
+// $(".fretboard").on('change', 'input', function (e) {
+//   // when an input is changed, update the list of pressed notes and show the updated list
+//   clearOthersInRow(this);
+//   showSelected();
+// });
+// function showSelected() {
+//   // output a list of strings being pressed (for convenience in appending to chords array)
+//   var sel = getSelected();
+//   $("#selected").val(sel.length?"'"+sel.join("','")+"'":'');
+// }
+// function getSelected() {
+//   // get the array of notes being pressed
+//   var sel = [];
+//   $('.fretboard input:checked').each(function () {
+//     sel.push($(this).attr('id'));
+//   });
+//   return sel;
+// }
+// function clearOthersInRow(selected) {
+//   // ensure `selected` is the only note being pressed on this string
+//   var s = $(selected).attr('data-string');
+//   $('.fretboard div[data-string="'+s+'"] input')
+//       .not(selected).prop('checked', false);
+// }
+//
+// // initialize
+// showSelected();
+// });
